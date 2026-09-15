16ac582c1e221123eefb76005e9ef01f89b90ecf
1205417239 <17757095796@163.com>
2026-09-16 07:07:03 +0800

    Delete KTClipboardViewController.h

--- KTClipboardViewController.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-@interface KTClipboardViewController : UIViewController
-- (instancetype)initWithInput:(id<UITextInput>)input;
-@property(nonatomic,copy) void (^closeHandler)(void);
-@property(nonatomic,strong) UIView *panel;
-@end
