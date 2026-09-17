267a130fa739aaaaf435d9954431954b5958d504
1205417239 <17757095796@163.com>
2026-09-17 12:05:53 +0800

    Add files via upload

--- KTDebugViewController.m
+++ KTDebugViewController.m
@@ -1,23 +1,37 @@
 #import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
 #import "KTDebugLogger.h"
+
 @interface KTDebugViewController : PSListController
+@property(nonatomic,strong) UITextView *logView;
 @end
+
 @implementation KTDebugViewController
 - (NSArray *)specifiers { return @[]; }
 - (void)loadView {
+    UITableView *table=[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
     UITextView *v=[[UITextView alloc] initWithFrame:CGRectZero];
-    v.editable=NO; v.selectable=YES;
+    v.editable=NO;
+    v.selectable=YES;
     v.font=[UIFont monospacedSystemFontOfSize:12.0 weight:UIFontWeightRegular];
     v.text=KTDebugLogText();
     v.backgroundColor=UIColor.systemBackgroundColor;
     v.textColor=UIColor.labelColor;
-    self.view=v;
+    v.translatesAutoresizingMaskIntoConstraints=NO;
+    [table addSubview:v];
+    [NSLayoutConstraint activateConstraints:@[
+        [v.topAnchor constraintEqualToAnchor:table.topAnchor],
+        [v.leadingAnchor constraintEqualToAnchor:table.leadingAnchor],
+        [v.trailingAnchor constraintEqualToAnchor:table.trailingAnchor],
+        [v.bottomAnchor constraintEqualToAnchor:table.bottomAnchor]
+    ]];
+    self.logView=v;
+    self.view=table;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title=@"调试日志";
     self.navigationItem.rightBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:@"清空" style:UIBarButtonItemStylePlain target:self action:@selector(clearLog)];
 }
-- (void)clearLog { KTDebugLogClear(); ((UITextView *)self.view).text=@"暂无调试记录"; }
+- (void)clearLog { KTDebugLogClear(); self.logView.text=@"暂无调试记录"; }
 @end
