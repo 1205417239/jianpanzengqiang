6214eb4f9ec768c76b483154b0466f7d457da21b
1205417239 <17757095796@163.com>
2026-09-17 11:42:52 +0800

    Add files via upload

--- KTDebugLogger.h
+++ KTDebugLogger.h
@@ -1,4 +1,12 @@
 #import <Foundation/Foundation.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void KTDebugLog(NSString *format, ...);
 NSString *KTDebugLogText(void);
+
+#ifdef __cplusplus
+}
+#endif
